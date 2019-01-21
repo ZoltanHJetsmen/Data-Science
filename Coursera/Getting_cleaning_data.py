3ba0f435447aa28b6dc728033590284a4ebd89f2
ZoltanHJetsmen <zoltanhjetsmen@gmail.com>
2019-01-21 01:31:41 -0200

    New functions - Getting_clear_data.py

--- Coursera/Getting_cleaning_data.py
+++ Coursera/Getting_cleaning_data.py
@@ -3,7 +3,7 @@
 import os
 import pandas as pd
 import requests
-import xml.etree.ElementTree as et  
+import xml.etree.ElementTree as et
 
 from urllib.request import urlopen
 
@@ -29,19 +29,72 @@ def readExcelFiles():
 
 # Source: https://docs.python.org/2/library/xml.etree.elementtree.html 
 def readXMLFiles():
-    
-    tree = et.parse('./datasets/dataset.xml')  
+
+    tree = et.parse('./datasets/dataset.xml')
     root = tree.getroot()
-    
 
-    
+def readJSON():
+	
+	import json
+	
+	# Open the json file
+	with open("datasets/dataset.json") as f:
+		data = json.load(f)
+		# Print the key's value
+		print(data[0]["id"])
+
+# Source: https://www.tutorialspoint.com/python3/python_database_access.htm
+#		  https://stackoverflow.com/questions/3556305/how-to-retrieve-table-names-in-a-mysql-database-with-python-and-mysqldb
+#    	  https://stackoverflow.com/questions/23786674/python-mysqldb-how-to-get-columns-name-without-executing-select-in-a-big-tab
+def readMySQL(url, user, password):
+
+	import pymysql
+
+	db = pymysql.connect(url, user, password)
+
+	cursor = db.cursor()
+
+	# Show all the datatbases
+	cursor.execute("show databases")
+	for databases in cursor:
+		print(databases[0])
+ 
+	# Show all the tables from database "hg19"
+	cursor.execute("use hg19")
+	cursor.execute("show tables")
+	for tables in cursor:
+		print(tables[0])
+
+	# Show the columns from table affyU133Plus2
+	cursor.execute("show columns from affyU133Plus2")
+	for names in cursor:
+		print(names)
+
+	# Show all data from table affyU133Plus2
+	cursor.execute("select * from affyU133Plus2")
+	for names in cursor:
+		print(names)
+
+	# Close the connection
+	db.close()
+
+def webScraping():
+
+	with urlopen("https://scholar.google.com/citations?user=HI-I6C0AAAAJ&hl=en") as testfile:
+		html = testfile.read().decode()
+
+	print(html)
+	tree = et.fromstring("<html><head><title>Jeff Leek - Google Scholar Citations</title></head></html>")
 
 
-url = "https://data.baltimorecity.gov/api/views/dz54-2aru/rows.csv?accessType=DOWNLOAD"
+#url = "https://data.baltimorecity.gov/api/views/dz54-2aru/rows.csv?accessType=DOWNLOAD"
+#url = "http://api.github.com/users/jtleek/repos"
 #downloadFile(url, "dataset", ".csv")
 #readLocalFiles()
 #readXMLFiles()
-
+#readJSON()
+#readMySQL("genome-mysql.soe.ucsc.edu", "genomep", "password")
+webScraping()
 
 
 
